4332b347f991745aa151d98306a9a1d3830e6210
springwings <seo@5lulu.com>
2024-08-15 14:25:14 +0800

    update

--- src/views/system/Logs.vue
+++ src/views/system/Logs.vue
@@ -5,14 +5,14 @@
   </div>
   <div class="flex flex-right">
     <el-input style="width: 100px" placeholder="返回日志行数" v-model="log_lines" clearable :maxlength="12"></el-input>
-     <el-select style="width: 300px" placeholder="请选择节点IP" v-model="ip" clearable>
+     <el-select style="width: 300px" placeholder="请选择节点IP,默认主节点" v-model="ip" clearable>
         <el-option v-for="ip in hosts"  :key="ip"  :label="ip"  :value="ip"></el-option>
      </el-select>
    <el-button  @click="startLogPolling" style="width:150px" type="primary">节点实时日志</el-button>
   <el-button  @click="handleError" style="width:150px" type="info">节点错误日志</el-button>
   </div>
   </div>
-  <div style="border:solid 1px #ccc;margin-top:20px;position: relative;">
+  <div style="border:solid 1px #ccc;margin-top:20px;padding-bottom:10px;position: relative;background: #f3f3f3;">
     <codemirror v-model="code" :options="cmOptions" style="margin-top:10px;" ref="codeMirror" @scroll="handleScroll"></codemirror>
   </div>
   </el-card >
@@ -139,7 +139,7 @@ export default {
 
 <style lang="scss" scoped>
 ::v-deep .CodeMirror {
-  height: 600px;
+  height: 600px;background: #f9fff3;
 }
 ::v-deep .CodeMirror .cm-success {
   color: #2fb300;
